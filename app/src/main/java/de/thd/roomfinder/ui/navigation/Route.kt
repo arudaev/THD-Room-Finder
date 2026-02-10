ff01381263568acb029aae53baf6ce2998bea3c3
HlexNC <hlexhelftd@gmail.com>
2026-02-10 09:54:56 +0100

    Wire up RoomDetail navigation route

--- app/src/main/java/de/thd/roomfinder/ui/navigation/Route.kt
+++ app/src/main/java/de/thd/roomfinder/ui/navigation/Route.kt
@@ -3,4 +3,8 @@ package de.thd.roomfinder.ui.navigation
 sealed class Route(val route: String) {
     data object Home : Route("home")
     data object RoomList : Route("room_list")
+    data object RoomDetail : Route("room_detail/{roomId}/{dateTimeEpoch}") {
+        fun createRoute(roomId: Int, dateTimeEpoch: Long): String =
+            "room_detail/$roomId/$dateTimeEpoch"
+    }
 }
