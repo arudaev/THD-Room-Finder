205e3f3a5cfacaaa6ee454e77055a8429dbd853d
arudaev <hlexhelftd@gmail.com>
2026-07-07 19:55:34 +0200

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { Capacitor } from '@capacitor/core';
+import { Analytics } from '@vercel/analytics/react';
 import App from './App';
 import './styles/index.css';
+
+const shouldEnableVercelAnalytics = !Capacitor.isNativePlatform();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      {shouldEnableVercelAnalytics ? <Analytics /> : null}
     </BrowserRouter>
   </StrictMode>,
 );
